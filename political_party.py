44e9ebbca5bff762158fa6d5d86b1c24eab0ea28
Damon Lin <damonlin00@gmail.com>
2021-05-08 14:50:37 -0400

    Updates to Python file

--- political_party.py
+++ political_party.py
@@ -13,7 +13,6 @@ U.S. Census Bureau QuickFacts: United States, from
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import optimize
-import sys
 from tqdm import tqdm
 
 
@@ -38,7 +37,7 @@ def load_data_file(filename):
                 data.append(nums)
         return np.array(data)
     except FileNotFoundError:
-        print(f"{filename} does not exist in this directory.", file=sys.stderr)
+        print(f"{filename} does not exist in this directory.")
 
 
 def load_results_file(filename):
@@ -58,7 +57,7 @@ def load_results_file(filename):
                 results = np.append(results, int(line.strip()))
         return results
     except FileNotFoundError:
-        print(f"{filename} does not exist in this directory.", file=sys.stderr)
+        print(f"{filename} does not exist in this directory.")
 
 
 def sigmoid(X):
@@ -71,11 +70,7 @@ def sigmoid(X):
     Return value:
         The sigmoid of the input, which can be an integer or a NumPy array
     """
-    try:
-        return 1 / (1 + np.e ** (-X))
-    except TypeError:
-        error = "Only numbers and NumPy arrays allowed as input."
-        print(error, file=sys.stderr)
+    return 1 / (1 + np.e ** (-X))
 
 
 def compute_cost(theta, X, y, reg_const):
@@ -91,16 +86,13 @@ def compute_cost(theta, X, y, reg_const):
     Return value:
         the cost of the parameters
     """
-    try:
-        n = len(y)
-        h = sigmoid(np.matmul(X, theta))
-        x1 = np.matmul(np.transpose(y), np.log(h))
-        x2 = np.matmul(np.transpose(1 - y), np.log(1 - h))
-        c_reg = reg_const * sum(theta[1:] ** 2) / (2 * n)
-        cost = - (x1 + x2) / n + c_reg
-        return cost
-    except ValueError:
-        print("Dimension mismatch.", file=sys.stderr)
+    n = len(y)
+    h = sigmoid(np.matmul(X, theta))
+    x1 = np.matmul(np.transpose(y), np.log(h))
+    x2 = np.matmul(np.transpose(1 - y), np.log(1 - h))
+    c_reg = reg_const * sum(theta[1:] ** 2) / (2 * n)
+    cost = - (x1 + x2) / n + c_reg
+    return cost
 
 
 def compute_grad(theta, X, y, reg_const):
@@ -116,19 +108,16 @@ def compute_grad(theta, X, y, reg_const):
     Return value:
         the gradient of the parameters
     """
-    try:
-        n = len(y)
-        h = sigmoid(np.matmul(X, theta))
-        g_theta = np.copy(theta)
-        g_theta[0] = 0
-        g_reg = reg_const * g_theta / n
-        grad = np.matmul(np.transpose(X), h - y) / n + g_reg
-        return grad
-    except ValueError:
-        print("Dimension mismatch.", file=sys.stderr)
+    n = len(y)
+    h = sigmoid(np.matmul(X, theta))
+    g_theta = np.copy(theta)
+    g_theta[0] = 0
+    g_reg = reg_const * g_theta / n
+    grad = np.matmul(np.transpose(X), h - y) / n + g_reg
+    return grad
 
 
-def gradient_descent(X, y, alpha, num_iter):
+def gradient_descent(X, y, lr, num_iter):
     """
     Iterate gradient descent on the parameters to minimize cost as well as
     gathering a cost_vec vector to be used for plotting the cost as a function
@@ -137,41 +126,32 @@ def gradient_descent(X, y, alpha, num_iter):
     Arguments:
         X: an n x m matrix of floats of the test data
         y: an n x 1 vector of 1's (Democrat) and 0's (Republican)
-        alpha: the learning rate
+        lr: the learning rate
         num_iter: the number of times to iterate gradient descent
 
     Return value:
-        a (theta, cost_vec) tuple of the parameters after gradient descent and
+        a (theta, loss_vec) tuple of the parameters after gradient descent and
         the vector of the cost after each iteration
     """
-    try:
-        m = X.shape[1]
-        theta = np.zeros(m, dtype=float)
-        reg_const = 0.05
-        cost_vec = np.array([])
-        convergence = False
-        improvement = 1e-9
-        for _ in tqdm(range(num_iter)):
-            old_error = compute_cost(theta, X, y, reg_const)
-            cost_vec = np.append(cost_vec, old_error)
-            theta -= alpha * compute_grad(theta, X, y, reg_const)
-            new_error = compute_cost(theta, X, y, reg_const)
-            convergence = old_error - new_error < improvement
-            if old_error < new_error:
-                raise ValueError("Learning rate is too large.")
-            if convergence:
-                break
-        return (theta, cost_vec)
-    except ValueError as e:
-        print(e, file=sys.stderr)
+    m = X.shape[1]
+    theta = np.zeros(m, dtype=float)
+    reg_const = 0.05
+    loss_vec = np.array([])
+    is_close = 1e-9
+    for _ in tqdm(range(num_iter)):
+        old_error = compute_cost(theta, X, y, reg_const)
+        loss_vec = np.append(loss_vec, old_error)
+        theta -= lr * compute_grad(theta, X, y, reg_const)
+        new_error = compute_cost(theta, X, y, reg_const)
+        convergence = old_error - new_error < is_close
+        if convergence:
+            break
+    return theta, loss_vec
 
 
 def run_gradient_descent():
     """
     Run gradient descent on the specified text files.
-
-    Arguments:
-        none
 
     Return value:
         a (theta, cost_vec) tuple of the parameters after gradient descent and
@@ -181,34 +161,28 @@ def run_gradient_descent():
     # note that the figures in the text file has been rescaled so that all
     # values are between 0 and 10 for faster convergence
     y = load_results_file("training_party_affiliation.txt")
-    a, n = 0.1, 40000
-    return gradient_descent(X, y, a, n)
+    lr, num_iter = 0.1, 40000
+    return gradient_descent(X, y, lr, num_iter)
 
 
-def plot_cost():
+def plot_loss(vector):
     """
-    Plot the cost of the parameters as a function of the number of iterations
+    Plot the loss of the parameters as a function of the number of iterations
     done by gradient descent.
 
-    Arguments:
-        none
-
-    Return value:
-        none
+    Argument:
+        vector: the vector of loss to plot
     """
-    plt.plot(cost_vec)
+    plt.plot(vector)
     plt.title("Gradient descent")
     plt.xlabel("Iterations")
-    plt.ylabel("Cost")
+    plt.ylabel("Loss")
     plt.show()
 
 
 def run_bfgs():
     """
     Run BFGS on the specified text files to find the optimal parameters.
-
-    Arguments:
-        none
 
     Return value:
         the optimal parameters after running BFGS
@@ -218,9 +192,7 @@ def run_bfgs():
     # values are between 0 and 10 for faster convergence
     y = load_results_file("training_party_affiliation.txt")
     t0 = np.zeros(X.shape[1], dtype=float)
-    arguments = (X, y, 0.05)
-    theta = optimize.minimize(compute_cost, t0, method="bfgs",
-                              jac=compute_grad, args=(X, y, 0.05))
+    theta = optimize.minimize(compute_cost, t0, method="bfgs", jac=compute_grad, args=(X, y, 0.05))
     return theta.x
 
 
@@ -228,9 +200,6 @@ def accuracy():
     """
     Test the accuracy of the parameters obtained from gradient descent and BFGS
     on training and test data data.
-
-    Arguments:
-        none
 
     Return value:
         a (gd_train_acc, bfgs_train_acc, gd_test_acc, bfgs_test_acc) tuple
@@ -247,18 +216,12 @@ def accuracy():
     bfgs_train_acc = sum(np.round(h_train2) == y_train) / len(y_train) * 100
     gd_test_acc = sum(np.round(h_test1) == y_test) / len(y_test) * 100
     bfgs_test_acc = sum(np.round(h_test2) == y_test) / len(y_test) * 100
-    return (gd_train_acc, bfgs_train_acc, gd_test_acc, bfgs_test_acc)
+    return gd_train_acc, bfgs_train_acc, gd_test_acc, bfgs_test_acc
 
 
 def interactive_predict():
     """
     Predict which party a district will vote for by asking for user input.
-
-    Arguments:
-        none
-
-    Return value:
-        none
     """
     try:
         name = input("County or state name: ").capitalize()
@@ -277,44 +240,11 @@ def interactive_predict():
             chance = (1 - party) * 100
             print(f"{name}: {chance:.2f}% probability of voting Republican.")
     except ValueError:
-        print("All values entered should be numbers.", file=sys.stderr)
-
-
-def file_predict(filename):
-    """
-    Predict which party a district will vote for using information loaded from
-    a text file.
-
-    Arguments:
-        filename: the name of the text file containing the information
-
-    Return value:
-        none
-    """
-    try:
-        with open(filename) as f:
-            for line in f:
-                line_lst = line.split()
-                inflation = 1.152  # adjust from 2018 to 2010 US dollar
-                name = line_lst[0]
-                x1 = float(line_lst[1]) / 10000 / inflation
-                x2 = float(line_lst[2]) / 100
-                x3 = float(line_lst[3]) / 100
-                x4 = float(line_lst[4]) / 100
-                info = [1, x1, x2, x3, x4]
-                h = sigmoid(np.dot(theta_gd, info))
-                if round(h):
-                    print(f"{name}: Democrat")
-                else:
-                    print(f"{name}: Republican")
-    except FileNotFoundError:
-        print(f"{filename} does not exist in this directory.", file=sys.stderr)
-    except ValueError or IndexError:
-        print("The file is not in the correct format.", file=sys.stderr)
+        print("All values entered should be numbers.")
 
 
 if __name__ == "__main__":
-    theta_gd, cost_vec = run_gradient_descent()
+    theta_gd, loss_vec = run_gradient_descent()
     theta_bfgs = run_bfgs()
     param1 = np.around(theta_gd, decimals=4)
     param2 = np.around(theta_bfgs, decimals=4)
@@ -325,5 +255,5 @@ if __name__ == "__main__":
     print(f"With BFGS, the parameters are {param2}\n"
           f"with a training set accuracy of {bfgs_train_acc:.2f}% "
           f"and a test set accuracy of {bfgs_test_acc:.2f}%.")
-    # plot_cost()
+    plot_loss(loss_vec)
     # interactive_predict()
